260a147dcbb006a3ed8f818f94c6cec6017c2c1a
drewtadams <drew@drewtadams.com>
2023-10-28 13:00:53 -0600

    added black/white option; added border mask;

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageTk
 from tkinter import ttk
+from util.img_proc import apply_img_mask
 import cv2
 import numpy as np
 import tkinter as tk
@@ -8,34 +9,67 @@ import tkinter as tk
 resize_size = (800, 1067)
 og_img = cv2.imread('images/20190723_183846_original.jpg')
 highlighted_img = cv2.imread('images/20190723_183846_highlighted.jpg')
+thresh_bin = cv2.THRESH_BINARY
+
+# apply background mask
+apply_img_mask((0, 0, 0), [og_img, highlighted_img])
 
 
-def refresh_img():
-    # get the threshold values
-    rl_val = int(red_thresh_low_val.get())
-    rh_val = int(red_thresh_high_val.get())
-    gl_val = int(green_thresh_low_val.get())
-    gh_val = int(green_thresh_high_val.get())
-    bl_val = int(blue_thresh_low_val.get())
-    bh_val = int(blue_thresh_high_val.get())
+def get_bw_img() -> np.ndarray:
+    # get threshold and max value
+    bwv_val = int(bw_thresh_val.get())
+    bwm_val = int(bw_thresh_max.get())
 
     # apply the threshold to the og image
-    _, r_bin_red = cv2.threshold(og_img[:, :, 2], rl_val, rh_val, cv2.THRESH_BINARY_INV)
-    _, r_bin_green = cv2.threshold(og_img[:, :, 1], gl_val, gh_val, cv2.THRESH_BINARY_INV)
-    _, r_bin_blue = cv2.threshold(og_img[:, :, 0], bl_val, bh_val, cv2.THRESH_BINARY_INV)
+    gray_img = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
+    bin_img = cv2.threshold(gray_img, bwv_val, bwm_val, cv2.THRESH_BINARY)[1]
+
+    # regenerate the side-by-side images
+    og_counted = cv2.resize(cv2.cvtColor(highlighted_img, cv2.COLOR_BGR2RGB), resize_size)
+    r_bin_img = cv2.resize(cv2.cvtColor(bin_img, cv2.COLOR_GRAY2RGB), resize_size)
+    return np.concatenate((og_counted, r_bin_img), axis=1)
+
+
+def get_rgb_img() -> np.ndarray:
+    # get the threshold and max values
+    rv_val = int(red_thresh_val.get())
+    rm_val = int(red_thresh_max.get())
+    gv_val = int(green_thresh_val.get())
+    gm_val = int(green_thresh_max.get())
+    bv_val = int(blue_thresh_val.get())
+    bm_val = int(blue_thresh_max.get())
+
+    # apply the threshold to the og image
+    _, r_bin_red = cv2.threshold(og_img[:, :, 2], rv_val, rm_val, thresh_bin)
+    _, r_bin_green = cv2.threshold(og_img[:, :, 1], gv_val, gm_val, thresh_bin)
+    _, r_bin_blue = cv2.threshold(og_img[:, :, 0], bv_val, bm_val, thresh_bin)
 
     # regenerate the side-by-side images
     og_counted = cv2.resize(cv2.cvtColor(highlighted_img, cv2.COLOR_BGR2RGB), resize_size)
     r_bin_img = cv2.resize(cv2.cvtColor(cv2.merge((r_bin_blue, r_bin_green, r_bin_red)), cv2.COLOR_BGR2RGB), resize_size)
-    r_h_concat = np.concatenate((og_counted, r_bin_img), axis=1)
+    return np.concatenate((og_counted, r_bin_img), axis=1)
+
+
+def refresh_img():
+    print(f'is_bw: {is_bw.get()}, {type(int(is_bw.get()))}')
+    refreshed_img = get_bw_img() if int(is_bw.get()) else get_rgb_img()
 
     # recreate the image widget
-    r_img_tk = ImageTk.PhotoImage(image=Image.fromarray(r_h_concat))
-    # label = ttk.Label(root, image=img_tk)
+    r_img_tk = ImageTk.PhotoImage(image=Image.fromarray(refreshed_img))
 
     # update the label
-    label.config(image=r_img_tk)
-    label.image = r_img_tk
+    primary_img.config(image=r_img_tk)
+    primary_img.image = r_img_tk
+
+
+def show_bw_ctrl():
+    rgb_thresh_frame.grid_forget()
+    bw_thresh_frame.grid(row=1, column=37, columnspan=6)
+
+
+def show_rgb_ctrl():
+    bw_thresh_frame.grid_forget()
+    rgb_thresh_frame.grid(row=1, column=37, columnspan=6)
 
 
 root = tk.Tk()
@@ -43,26 +77,33 @@ root.title('image thresholding')
 
 # threshold input fields
 validate_input = root.register(lambda action, field, value, reason, **kwargs: len(value) <= 3)
-red_thresh_low_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-red_thresh_high_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-red_thresh_low_val.insert(128, '128')
-red_thresh_high_val.insert(255, '255')
-green_thresh_low_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-green_thresh_high_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-green_thresh_low_val.insert(128, '128')
-green_thresh_high_val.insert(255, '255')
-blue_thresh_low_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-blue_thresh_high_val = ttk.Entry(root, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
-blue_thresh_low_val.insert(128, '128')
-blue_thresh_high_val.insert(255, '255')
 
-# refresh button
-refresh_btn = ttk.Button(root, text='refresh', command=refresh_img)
+# bw frame
+bw_thresh_frame = tk.Frame(root)
+bw_thresh_val = ttk.Entry(bw_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+bw_thresh_max = ttk.Entry(bw_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+bw_thresh_val.insert(65, '65')
+bw_thresh_max.insert(255, '255')
+
+# rgb frame
+rgb_thresh_frame = tk.Frame(root)
+red_thresh_val = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+red_thresh_max = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+red_thresh_val.insert(65, '65')
+red_thresh_max.insert(255, '255')
+green_thresh_val = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+green_thresh_max = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+green_thresh_val.insert(65, '65')
+green_thresh_max.insert(255, '255')
+blue_thresh_val = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+blue_thresh_max = ttk.Entry(rgb_thresh_frame, width=3, validate="key", validatecommand=(validate_input, '%d', '%W', '%P', '%v'))
+blue_thresh_val.insert(65, '65')
+blue_thresh_max.insert(255, '255')
 
 # apply the threshold to the og image
-_, bin_red = cv2.threshold(og_img[:, :, 2], 128, 255, cv2.THRESH_BINARY_INV)
-_, bin_green = cv2.threshold(og_img[:, :, 1], 128, 255, cv2.THRESH_BINARY_INV)
-_, bin_blue = cv2.threshold(og_img[:, :, 0], 128, 255, cv2.THRESH_BINARY_INV)
+_, bin_red = cv2.threshold(og_img[:, :, 2], 65, 255, thresh_bin)
+_, bin_green = cv2.threshold(og_img[:, :, 1], 65, 255, thresh_bin)
+_, bin_blue = cv2.threshold(og_img[:, :, 0], 65, 255, thresh_bin)
 
 # convert images from BGR to RGB and concatenate them
 highlighted_img_rgb = cv2.resize(cv2.cvtColor(highlighted_img, cv2.COLOR_BGR2RGB), resize_size)
@@ -71,27 +112,39 @@ h_concat = np.concatenate((highlighted_img_rgb, bin_img), axis=1)
 
 # load the image
 img_tk = ImageTk.PhotoImage(image=Image.fromarray(h_concat))
-label = ttk.Label(root, image=img_tk)
+primary_img = ttk.Label(root, image=img_tk)
+primary_img.grid(row=0, column=0, columnspan=80)
 
-# widget layout
-label.grid(row=0, column=0, columnspan=80)
+# bw frame layout
+ttk.Label(bw_thresh_frame, text='BLACK & WHITE (val | max):').grid(row=0, column=0)
+bw_thresh_val.grid(row=0, column=1)
+ttk.Label(bw_thresh_frame, text='|').grid(row=0, column=2)
+bw_thresh_max.grid(row=0, column=3)
 
-ttk.Label(root, text='RED (low | high):').grid(row=1, column=39)
-red_thresh_low_val.grid(row=1, column=40)
-ttk.Label(root, text='|').grid(row=1, column=41)
-red_thresh_high_val.grid(row=1, column=42)
+# rgb frame layout
+ttk.Label(rgb_thresh_frame, text='RED (val | max):').grid(row=0, column=0)
+red_thresh_val.grid(row=0, column=1)
+ttk.Label(rgb_thresh_frame, text='|').grid(row=0, column=2)
+red_thresh_max.grid(row=0, column=3)
+ttk.Label(rgb_thresh_frame, text='GREEN (val | max):').grid(row=1, column=0)
+green_thresh_val.grid(row=1, column=1)
+ttk.Label(rgb_thresh_frame, text='|').grid(row=1, column=2)
+green_thresh_max.grid(row=1, column=3)
+ttk.Label(rgb_thresh_frame, text='BLUE (val | max):').grid(row=2, column=0)
+blue_thresh_val.grid(row=2, column=1)
+ttk.Label(rgb_thresh_frame, text='|').grid(row=2, column=2)
+blue_thresh_max.grid(row=2, column=3)
 
-ttk.Label(root, text='GREEN (low | high):').grid(row=2, column=39)
-green_thresh_low_val.grid(row=2, column=40)
-ttk.Label(root, text='|').grid(row=2, column=41)
-green_thresh_high_val.grid(row=2, column=42)
+show_rgb_ctrl()
 
-ttk.Label(root, text='BLUE (low | high):').grid(row=3, column=39)
-blue_thresh_low_val.grid(row=3, column=40)
-ttk.Label(root, text='|').grid(row=3, column=41)
-blue_thresh_high_val.grid(row=3, column=42)
+# rgb/bw toggle
+is_bw = tk.StringVar()
+is_bw.set(0)
+ttk.Radiobutton(root, text='RGB', variable=is_bw, value=0, command=show_rgb_ctrl).grid(row=3, column=39)
+ttk.Radiobutton(root, text='B/W', variable=is_bw, value=1, command=show_bw_ctrl).grid(row=3, column=40)
 
-refresh_btn.grid(row=4, column=40)
+# refresh button
+ttk.Button(root, text='refresh', command=refresh_img).grid(row=4, column=39)
 
 # start tkinter
 root.mainloop()
---
+++ util/img_proc.py
@@ -0,0 +1,82 @@
+from typing import List, Tuple, Union
+import cv2
+import numpy as np
+
+
+CLOSE = 0
+OPEN = 1
+
+
+def apply_img_mask(
+        mask_color: Union[int, Tuple],
+        imgs: List,
+        min_foreground_area: int = 10000000,
+        max_foreground_area: int = 13000000,
+        threshold: int = 75):
+    """
+    in-place operation that fills the inverse foreground contour for each passed image
+    :param mask_color: (R, G, B) color or L luminance of the contour fill
+    :param imgs: list of images
+    :param min_foreground_area: minimum px area of the foreground in the image
+    :param max_foreground_area: maximum px area of the foreground in the image
+    :param threshold: minimum px threshold for black and white conversion
+    :return: None
+    """
+    for i, img in enumerate(imgs):
+        # convert the image to prep for closure
+        proc_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) > 2 else img  # grayscale
+        proc_img = cv2.GaussianBlur(proc_img, (3, 3), 0)  # gaussian blur
+        _, proc_img = cv2.threshold(proc_img, threshold, 255, cv2.THRESH_BINARY)  # black and white
+
+        # apply open -> close to remove noise then fill line gaps
+        proc_img = open_close(proc_img, OPEN)
+        proc_img = open_close(proc_img, CLOSE)
+
+        #
+        cur_min_contour = tuple()
+        contours = cv2.findContours(proc_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        for contour in contours:
+            # outline should only be within the range of the area for the piece of paper
+            if min_foreground_area < (contour_area := cv2.contourArea(contour)) < max_foreground_area:
+                if not cur_min_contour or (cur_min_contour and cur_min_contour[1] > contour_area):
+                    mask_val = 255
+                    stencil_shape = img.shape[:2]
+
+                    # get the bounding box
+                    rect = cv2.minAreaRect(contour)
+                    box = cv2.boxPoints(rect)
+                    box = np.int0(box)
+
+                    # fill everything BUT the bounding box
+                    stencil = np.zeros(stencil_shape).astype(np.uint8)
+                    cv2.fillPoly(stencil, [box], mask_val)
+                    inverse_mask = stencil != mask_val
+                    img[inverse_mask] = mask_color
+
+                    # keep track of the current smallest contour
+                    cur_min_contour = (contour, contour_area)
+
+        # update by reference
+        imgs[i] = img
+
+
+def open_close(img: np.ndarray, proc_enum: int, open_close_iterations: int = 5) -> np.ndarray:
+    """
+    applies the open or close process on a copy of the passed image and returns the results
+    :param img: image to have opening/closing processing done
+    :param proc_enum: precess enum belonging to the desired process (CLOSE or OPEN)
+    :param open_close_iterations: number of erosion/dilation iterations
+    :return: np.ndarray image
+    """
+    proc_funcs = [cv2.dilate, cv2.erode]
+    img_copy = img.copy()
+
+    # primary function call (dilate or erode)
+    for i in range(open_close_iterations):
+        img_copy = proc_funcs[proc_enum](img_copy, None, iterations=i+1)
+
+    # secondary function call (erode or dilate)
+    for i in range(open_close_iterations):
+        img_copy = proc_funcs[(proc_enum + 1) % len(proc_funcs)](img_copy, None, iterations=i+1)
+
+    return img_copy
